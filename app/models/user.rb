4ac0efb6ab824ef854b12e369b95045cf71181fd
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-02 22:25:10 +0900

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :skill, presence: true 
+  validates :skill, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字(半角)と数字(半角)の両方を含めて設定してください'
-
 end
