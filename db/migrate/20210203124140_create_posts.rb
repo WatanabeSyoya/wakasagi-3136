4559f0513facf76af774ff6a8ce88db7bfa54029
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-03 22:23:09 +0900

    postsテーブル作成

---
+++ db/migrate/20210203124140_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :title,                 null: false
+      t.text :post_text,               null: false
+      t.integer :place_id,             null: false
+      t.integer :fishing_result_id,    null: false
+      t.integer :water_depth_id,       null: false
+      t.integer :weather_id,           null: false
+      t.integer :feed_id,              null: false
+      t.references :user,              foreign_key: true
+      t.timestamps
+    end
+  end
+end
