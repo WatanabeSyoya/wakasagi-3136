fda7faca2337da1638ac5ea5409cf13760606246
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-07 16:48:12 +0900

    dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
-  has_many :messages
+  has_many :messages, dependent: :destroy
   
   validates :nickname, :skill, presence: true
 
