e80b59645ffd59f2867197b7635d57b1ede74a06
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-08 23:49:59 +0900

    スライド

--- app/javascript/slide.js
+++ app/javascript/slide.js
@@ -1,10 +1,12 @@
 $(function(){
   $('.bxslider').bxSlider({
+    speed: 4000,
     mode: 'fade',
     auto: true,
     autoControls: true,
     stopAutoOnClick: true,
     pager: true,
+    pause: 8000,
     slideWidth: 2000
   });
 });
