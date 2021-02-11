7ee62cdf501524d7aa54a5fee83a036ab4bd8a39
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-11 18:26:53 +0900

    検索機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,5 @@ class UsersController < ApplicationController
     @max_fishing_result = Post.where(user_id: @user.id).maximum(:fishing_result)
     @max_fishing_result_details = Post.find_by fishing_result: @max_fishing_result, user_id: @user.id
     @posts = @user.posts
-    @post = Post.find(params[:id])
   end
 end
