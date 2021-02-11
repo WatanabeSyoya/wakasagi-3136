eab510d88c5dc4acf41c03b7fbc1a121e3a99ead
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-12 00:55:39 +0900

    ユーザー詳細

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @fishing_result_sum = Post.where(user_id: @user.id).sum(:fishing_result)
     @max_fishing_result = Post.where(user_id: @user.id).maximum(:fishing_result)
     @max_fishing_result_details = Post.find_by fishing_result: @max_fishing_result, user_id: @user.id
     @posts = @user.posts
