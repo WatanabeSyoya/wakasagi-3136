acf576155bbe8236e52650a309b5095e96b8fe5f
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-12 00:26:34 +0900

    検索機能１

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require('jquery')
 require('../jquery.bxslider')
 require('../slide')
 require('../page_top')
+require('../post_search')
 global.$ = jQuery;
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,8 @@ Rails.application.routes.draw do
   root to: "posts#index"
   resources :posts do
     resources :messages, only: :create
-    collection do
-      get 'search'
-    end
   end
-  resources :users, only: :show
+  resources :users, only: :show 
   post   '/like/:post_id' ,to: 'likes#like',   as: 'like'
   delete '/like/:post_id' ,to: 'likes#unlike', as: 'unlike'
 end
