fadb63a7cf49bf0aef3882634d072c01a20fddeb
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-12 23:13:18 +0900

    スクロール

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,8 @@ require('jquery')
 require('../jquery.bxslider')
 require('../slide')
 require('../page_top')
-require('../post_search')
+require('../menu')
+
 global.$ = jQuery;
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
