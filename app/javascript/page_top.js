51cdea547aebbfcf02edc2b33f08e86ff78e41e6
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-13 14:08:20 +0900

    ハンバーガーメニュー作成

--- app/javascript/page_top.js
+++ app/javascript/page_top.js
@@ -2,7 +2,7 @@ jQuery(function() {
   var pagetop = $('#page_top');   
   pagetop.hide();
   $(window).scroll(function () {
-      if ($(this).scrollTop() > 630) {  //100pxスクロールしたら表示
+      if ($(this).scrollTop() > 250) {  //100pxスクロールしたら表示
           pagetop.fadeIn();
       } else {
           pagetop.fadeOut();
