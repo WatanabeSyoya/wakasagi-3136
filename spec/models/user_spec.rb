b4e8ac08177b9bd5def4a82713fa43de09c5e812
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-23 09:52:36 +0900

    ユーザー管理上手く行く時

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,8 +8,13 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     context '新規登録がうまくいくとき' do
       it 'すべての情報を入れると登録できる' do
+        @user
+        expect(@user).to be_valid
       end
       it 'パスワードは6文字以上であれば登録できる' do
+        @user.password = '123456a'
+        @user.password_confirmation = '123456a'
+        expect(@user).to be_valid
       end
     end
 
