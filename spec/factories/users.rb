d4039b0ca614451a839dd981ddae0e76b86a8094
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-23 09:43:17 +0900

    faker導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname                     { "ショウヤ" }
+    nickname                     { Faker::Name.last_name }
     email                        { Faker::Internet.free_email }
     password                     { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation        { password }
